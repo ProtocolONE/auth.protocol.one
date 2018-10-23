511b5871663e11523bc619bc5679d735ec1eb434
Вадим Сабиров <vadim.sabirov@syncopate.ru>
2018-10-23 10:45:30 +0300

    Remove console checking

--- public/index.php
+++ public/index.php
@@ -15,14 +15,6 @@ $isDev = SYMFONY_ENV_DEV === $environment;
 $debug = $isDev ? true : false;
 
 if ($isDev) {
-    if (isset($_SERVER['HTTP_CLIENT_IP'])
-        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-        || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
-    ) {
-        header('HTTP/1.0 403 Forbidden');
-        exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
-    }
-
     Debug::enable();
 }
 
